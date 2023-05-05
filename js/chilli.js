7cc4c3f99d2182868d5516d36829c6d072726052
Brian Kimbrow <bkimbrow25@gmail.com>
2023-05-04 23:04:45 -0500

    update

---
+++ js/chilli.js
@@ -0,0 +1,48 @@
+'use strict';
+
+let price = 6.99;
+
+let salesTax = 5.2;
+
+let discount = 10;
+
+let discount2 = 5;
+
+let shippingCost = 3.99;
+
+
+
+
+
+
+let dog1 = prompt("How many Hot Dogs?");
+
+let subtotal = price * dog1;
+
+alert(subtotal);
+
+
+let tax = saleTax / 100 * price;
+
+let total = subtotal + tax;
+
+alert(total);
+
+
+let discount1 = price / 1 + salesTax;
+
+let discountedPrice1 = subtotal +tax + discountedPrice1;
+
+alert(discountedPrice1);
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/mediumdog.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let salePrice = 6.99;
+
+let saleTax = 5.2;
+
+let discount1 = 10;
+
+let discount2 = 5;
+
+let shippingCost =  3.99;
+
+let total = 25;
+
+if (total > 25){
+  console.log(total);
+} 
