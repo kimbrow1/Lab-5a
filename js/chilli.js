4c5eec69314e6d453847f74812b2591b8bd17017
Brian Kimbrow <bkimbrow25@gmail.com>
2023-05-06 11:12:21 -0500

    new update

--- js/chilli.js
+++ js/chilli.js
@@ -15,6 +15,8 @@ let shippingCost = 3.99;
 
 
 
+
+
 let dog1 = prompt("How many Hot Dogs?");
 
 let subtotal = price * dog1;
@@ -22,7 +24,7 @@ let subtotal = price * dog1;
 alert(subtotal);
 
 
-let tax = saleTax / 100 * price;
+let tax =  price * saleTax / 100;
 
 let total = subtotal + tax;
 
@@ -31,15 +33,21 @@ alert(total);
 
 let discount1 = price / 1 + salesTax;
 
-let discountedPrice1 = subtotal +tax + discountedPrice1;
+let discountedPrice1 = subtotal + tax + discountedPrice1;
 
 alert(discountedPrice1);
 
 
+let shippingCost;
 
+if (total >= 25) {
+    message = "Your shipping will be free"
+    shippingCost = 0;
+}else {
+    message = "The cost of shipping is 3.99"
 
-
-
+    shippingCost= 3.99
+}
 
 
 
